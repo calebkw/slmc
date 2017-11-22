0ef1e9c93c88f22e6d07dc05a2a67946b6ea30a6
caleb <calebkw94@gmail.com>
2017-11-22 13:21:12 -0600

    first pass at barebones image class

---
+++ Image.py
@@ -0,0 +1,18 @@
+class Image:
+
+    """Image class for holding image data and metadata"""
+
+    def __init__(self, im_data):
+        """
+
+        :param im_data: mxn array of image pixel data
+        """
+
+        self.image = im_data
+
+
+    def display(self):
+        # display image?
+
+
+
