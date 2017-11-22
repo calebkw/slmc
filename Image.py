cb3b156930980e222992599342bc5bae3151311e
caleb <calebkw94@gmail.com>
2017-11-22 13:49:06 -0600

    size method and filename var

--- Image.py
+++ Image.py
@@ -2,17 +2,26 @@ class Image:
 
     """Image class for holding image data and metadata"""
 
-    def __init__(self, im_data):
+    def __init__(self, im_data, filename):
         """
 
         :param im_data: mxn array of image pixel data
+        :param filename: str, name of file
         """
 
         self.image = im_data
+        self.name = filename
 
 
     def display(self):
+
         # display image?
+
+    def size(self):
+        numrows = len(self.image)
+        numcols = len(self.image[0])
+
+        return [numrows, numcols]
 
 
 
