0462a0a1ee3428cdf821941c888d86816c00ddcc
Tim Hoer <timothy.hoer@duke.edu>
2017-12-01 20:14:19 -0500

    closes #17

--- classification_output.py
+++ classification_output.py
@@ -1,16 +1,15 @@
-def write_to_file(output_filename, classified_output, uuid, date):
+def write_to_file(output_filename, classified_output, image_filename, date):
     """ Writes the output to a seperate file
     :param output_filename: str,name for output
     :param classified_output: The classification of the output file
-    :param uuid: Unique patient identifier
+    :param image_filename: Unique patient identifier
     :param date: Date when the request for classification was made
 
     """
-    #import output>>>
 
     file = open(output_filename, 'w')
     file.write('image: ')
-    file.write(str(uuid))
+    file.write(str(image_filename))
     file.write(', ')
     file.write('classification: ')
     file.write(str(classified_output))
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import platform
 import os
 from extract import extract
+from api_send import classify
+from classification_output import write_to_file
+from datetime import datetime
 
 
 def start_classifier():
@@ -21,14 +24,26 @@ def start_classifier():
                 print('Image extraction failed. Closing program.')
                 return
             # issue API request
-
+            try:
+                classified_array = classify(image_array)
+            except SystemError:
+                print('Image classification failed. Closing program.')
+                return
             # write output to file
-
+            try:
+                for image in classified_array:
+                    write_to_file('classification.out', image.output,
+                                  image.filename, datetime.now().strftime(
+                                   '%Y-%m-%d %H:%M:%S'))
+            except SystemError:
+                print('Writing classifications to file failed. Closing '
+                      'program.')
+                return
+        else:
+            print('NO DRIVE FOUND AT SPECIFIED LOCATION.')
 
 
 def hasdrive(letter):
     return "Windows" in platform.system() and os.system(
         "vol %s: 2>nul>nul" % (letter)) == 0
 
-if __name__ == '__main__':
-    print(start_classifier())
