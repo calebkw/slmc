8f234ce41753739fb434302807bb25b2c7d8bb9d
caleb <calebkw94@gmail.com>
2017-12-05 15:59:50 -0500

    typo oops

--- api_send.py
+++ api_send.py
@@ -1,4 +1,4 @@
-import requests
+import request
 import json
 
 
@@ -20,8 +20,9 @@ def classify(images_in):
     endpoint = 'http://vcm-1612.vm.duke.edu:5000/api/classify'
 
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-    r = requests.post(endpoint, data=json.dumps(jsn_dict), headers=headers)
+    r = request.post(endpoint, data=json.dumps(jsn_dict), headers=headers)
 
+    # TODO: parse return json and save classification to each object
     return r.json()
 
 
@@ -29,8 +30,7 @@ def num_requests():
     """Get total number of requests to server"""
 
     endpoint = 'http://vcm-1612.vm.duke.edu:5000/api/num_requests'
-
-    r = requests.get(endpoint)
+    r = request.get(endpoint)
 
     return r.json()
 
