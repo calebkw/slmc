e948a0a8a916894a97c931c89cd683783083f3d8
Tim Hoer <timothy.hoer@duke.edu>
2017-12-10 02:22:48 -0500

    adding OS compatibility

--- main.py
+++ main.py
@@ -12,55 +12,69 @@ def start_classifier():
     """
 
     while True:
-        drive = input("Enter USB drive location or \'quit\' to exit: ").strip()
-        if drive == 'quit':
-            break
-        elif hasdrive(drive):
-            # extract images from drive
-            try:
-                image_array = extract(drive + ':')
-                print('Images extracted: ' + str(len(image_array)))
-            except SystemError:
-                print('Image extraction from drive failed. Closing '
-                      'program.')
-                return
-            # issue API request
-            if len(image_array) > 0:
-                try:
-                    request = api_send.classify(image_array)
-                    classified_array = request[0]
-                    status_code = request[1]
-                except:
-                    print('System server failure. Closing program.')
-                    return
-                # write output to file
-                if status_code != 200:
-                    print('API request for image classification failed. '
-                          'Status code ' + str(status_code) + ".")
-                else:
-                    try:
-                        for lesion in classified_array['classified']:
-                            prob = lesion['prediction'][1][1]
-                            if prob > 0.50:
-                                outcome = lesion['prediction'][0][1]
-                            elif lesion['prediction'][1][0] > 0.50:
-                                prob = lesion['prediction'][1][0]
-                                outcome = lesion['prediction'][0][0]
-                            else:
-                                prob = 1.00
-                                outcome = 'indeterminate'
-                            write_to_file('classification.out', outcome, prob,
-                                          lesion['name'],
-                                          datetime.now().strftime(
-                                           '%Y-%m-%d %H:%M:%S'))
-                        print('Classifications sucessfully appended to '
-                              '\'classification.out\'.')
-                    except:
-                        print('Writing classifications to file failed. Closing'
-                              ' program.')
-                        return
+        if platform.system() is "Windows":
+            location = input("Enter directory or USB drive letter from which "
+                             "to extract images or \'quit\' to exit: ").strip()
+            if location == 'quit':
+                break
+            if len(location) == 1 and not hasdrive(location):
+                print("Drive could not be found.")
+                break
+            if len(location) == 1:
+                location = location + ':'
+        elif platform.system() is "Linux":
+            resp = input("Press enter to search for drive or \'quit\' to "
+                         "exit: ").strip()
+            if resp == 'quit':
+                break
+            print(os.listdir('/media/pi'))
         else:
-            print("No drive found at specified location.")
+            location = input("Enter directory from which "
+                             "to extract images or \'quit\' to exit: ").strip()
+            if location == 'quit':
+                break
+        try:
+            image_array = extract(location)
+            print('Images extracted: ' + str(len(image_array)))
+        except SystemError:
+            print('Image extraction from drive failed. Closing '
+                  'program.')
+            return
+        # issue API request
+        if len(image_array) > 0:
+            try:
+                request = api_send.classify(image_array)
+                classified_array = request[0]
+                status_code = request[1]
+            except:
+                print('System server failure. Closing program.')
+                return
+            # write output to file
+            if status_code != 200:
+                print('API request for image classification failed. '
+                      'Status code ' + str(status_code) + ".")
+            else:
+                try:
+                    for lesion in classified_array['classified']:
+                        prob = lesion['prediction'][1][1]
+                        if prob > 0.50:
+                            outcome = lesion['prediction'][0][1]
+                        elif lesion['prediction'][1][0] > 0.50:
+                            prob = lesion['prediction'][1][0]
+                            outcome = lesion['prediction'][0][0]
+                        else:
+                            prob = 1.00
+                            outcome = 'indeterminate'
+                        write_to_file('classification.out', outcome, prob,
+                                      lesion['name'],
+                                      datetime.now().strftime(
+                                       '%Y-%m-%d %H:%M:%S'))
+                    print('Classifications sucessfully appended to '
+                          '\'classification.out\'.')
+                except:
+                    print('Writing classifications to file failed. Closing'
+                          ' program.')
+                    return
 
 
 def hasdrive(letter):
